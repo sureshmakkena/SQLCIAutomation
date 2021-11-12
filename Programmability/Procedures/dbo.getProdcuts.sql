c6a163d1aedb09d9c462b2e6441b3815950056ff
MS2018 <makkenasbabu@gmail.com>
2021-11-12 23:47:07 +0530

    STORED PROCEDURE getProducts ADDED

---
+++ Programmability/Procedures/dbo.getProdcuts.sql
@@ -0,0 +1,13 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[getProdcuts]
+AS
+BEGIN
+	SELECT * FROM DBO.Products;
+END
+GO
