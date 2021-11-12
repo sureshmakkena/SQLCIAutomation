609124f22f3a869e7658bbaafa01c678bc0de301
MS2018 <makkenasbabu@gmail.com>
2021-11-12 23:42:10 +0530

    getAssets Stored Procedure Added.

---
+++ Programmability/Procedures/dbo.getAssets.sql
@@ -0,0 +1,17 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[getAssets]
+	
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	
+	SELECT * FROM dbo.ASSETS;
+END
+GO
